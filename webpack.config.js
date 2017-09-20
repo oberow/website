9fe3d079f72b2f79e16bfec244800dac735d81a3
walter goh <timwaltergoh@gmail.com>
2017-09-20 21:34:13 +0800

    set up starter kit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    entry: [
+        './source/App.js'
+    ],
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/, //match .js or .jsx files ?= 0 or 1
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }]
+    },
+    devServer: {
+        // contentBase: "./source", //get files from where
+        historyApiFallback: true,
+        inline: true
+    }
+};
