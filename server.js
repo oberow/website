e31b0d9aa98fbf8eaf52aa333485cde0481e489e
walter goh <timwaltergoh@gmail.com>
2017-10-15 00:22:09 +0800

    add heroku port to server.js

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(path.join(__dirname,"/dist")));
 
 console.log(path.join(__dirname,"/dist"));
 // app.use(express.static(__dirname));
 
-app.listen(7777,function(){
-    console.log("Started listening on port", 7777);
+app.listen(app.get('port'),function(){
+    console.log("Started listening on port", app.get('port'));
 })
