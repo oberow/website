31c0c1592342c0491d55749ac50846cc72b769a9
timothy_goh <timothy_goh@JOLENE>
2017-11-15 16:37:30 +0800

    book index page now reads the .md files and lists them

--- source/controllers/noteController.js
+++ source/controllers/noteController.js
@@ -1,7 +1,11 @@
 // var Author = require('../models/author');
 // var async = require('async');
 // var Book = require('../models/book');
+var path = require('path');
 
-exports.index = function(req,res){
-    res.send("NOT IMPLEMENTED: note index");
+var fs = require('fs');
+var files = fs.readdirSync(path.join(__dirname, '../note'));
+
+exports.index = function (req, res) {
+    res.render('pages/note/index', { books: files });
 };
