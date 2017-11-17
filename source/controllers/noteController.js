3aee1d8ad47477f0bf979e885ee8a4bf8a33d61b
timothy_goh <timothy_goh@JOLENE>
2017-11-17 08:15:13 +0800

    added markdown conversion to html for a hardcoded url

--- source/controllers/noteController.js
+++ source/controllers/noteController.js
@@ -5,18 +5,32 @@ var path = require('path');
 
 var fs = require('fs');
 
-var showdown  = require('showdown');
+var showdown = require('showdown');
 // converter = new showdown.Converter(),
 // text      = '#hello, markdown!',
 // html      = converter.makeHtml(text);
 
 var files = fs.readdirSync(path.join(__dirname, '../../note'));
+var noteDir = path.join(__dirname, '../../note');
 
 exports.index = function (req, res) {
-    res.render('pages/note/index', { books: files });
+    res.render('pages/note/index', {
+        books: files
+    });
 };
 
 exports.title = function (req, res) {
-    let text = fs.readFile('') 
-    res.render('pages/note/index', { books: files });
+    console.log('tim wasl here');
+    console.log(req.params.title);
+    let text = fs.readFile(noteDir + '/' + req.params.title + '.md', 'utf8', function (err, data) {
+
+        console.log(data);
+        let converter = new showdown.Converter();
+        let html = converter.makeHtml(data);
+        console.log('this is html', html);
+
+        res.render('pages/note/detail', {
+            html: html
+        });
+    })
 };
