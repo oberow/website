ed41280ae5280bf1f6fc96639186079184a8fe07
timothy_goh <timothy_goh@JOLENE>
2018-05-30 13:56:32 +0800

    added comment to server.js explaining what basedir option is for

--- server.js
+++ server.js
@@ -7,18 +7,22 @@ var home = require('./source/routes/home');
 
 var app = express();
 
+//set port
 app.set('port', (process.env.PORT || 5000));
 
+//set static
 app.use(express.static(path.join(__dirname, 'public')));
 
 //view engine setup
 app.set('views', __dirname + '/source');
 app.set('view engine', 'pug');
+//basedir is prepended to includes if the includes path is absolute, ie /root.pug instead of aoeu/head.pug
 app.locals.basedir = path.join(__dirname); //tell pug what the basedir is
 
 //use routes
 app.use('/', home);
 app.use('/note', note);
+
 
 app.listen(app.get('port'), function () {
     console.log("Started listening on port", app.get('port'));
